790d04db487436edfeed4235b7cf31e9ccf28b42
alanionita <a.g.ionita@gmail.com>
2017-06-02 12:19:50 +0100

    [_identity} adds tests for primitives and object types of input

--- spec/main.spec.js
+++ spec/main.spec.js
@@ -16,7 +16,72 @@ describe('_.identity', function () {
         let input = 'string';
         let actual = _.identity(input);
         let expected = 'string';
-        
+
         expect(actual).to.equal(expected);
+    });
+    describe('_.identity works for all primitive types', function () {
+        it('booleans', function () {
+            let input = true;
+            let actual = _.identity(input);
+            let expected = true;
+
+            expect(actual).to.equal(expected);
+        });
+        it('numbers', function () {
+            let input = 1267672673;
+            let actual = _.identity(input);
+            let expected = 1267672673;
+
+            expect(actual).to.equal(expected);
+        });
+        it('string', function () {
+            let input = 'string';
+            let actual = _.identity(input);
+            let expected = 'string';
+
+            expect(actual).to.equal(expected);
+        });
+        it('null', function () {
+            let input = null;
+            let actual = _.identity(input);
+            let expected = null;
+
+            expect(actual).to.equal(expected);
+        });
+        it('undefined', function () {
+            let input;
+            let actual = _.identity(input);
+
+            expect(actual).to.undefined;
+        });
+    });
+    describe('_.identity works for all objects', function () {
+        it('objects', function () {
+            let input = {
+                o: 'o',
+                b: 'b',
+                j: 'j',
+            };
+            let actual = _.identity(input);
+            let expected = {
+                o: 'o',
+                b: 'b',
+                j: 'j',
+            };
+            expect(actual).to.eql(expected);
+        });
+        it('functions', function () {
+            let input = function (param) { return param; };
+            let actual = _.identity(input);
+
+            expect(actual).to.equal(input);
+        });
+        it('arrays', function () {
+            let input = ['a', 'r', 'r', 'a', 'y'];
+            let actual = _.identity(input);
+            let expected = ['a', 'r', 'r', 'a', 'y'];
+
+            expect(actual).to.eql(expected);
+        });
     });
 });
