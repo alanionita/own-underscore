64a193efaba004e8b5e96c4084917e19dc490226
alanionita <a.g.ionita@gmail.com>
2017-07-10 03:54:48 +0100

    added advanced tasks todo

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 const _ = {};
 // Tasks
-// 9. map
-// 10. contains
+// 1. once
+// 2. memoize
+// 3. shuffle
+// 4. invoke
+// 5. sortBy (NB the Underscore library uses the native JavaScript sort but feel free to use your sort algorithm!)
+// 6. zip
+// 7. sortedIndex
+// 8. flatten
+// 9. intersection
+// 10. difference
+// 11. indexOf (again, this time with a binary search)
+// 12. throttle
+// 13. delay
 
 _.identity = function (args) {
     return args;
