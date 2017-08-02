669d462ef79085355a80c8b105841f154801503c
alanionita <a.g.ionita@gmail.com>
2017-08-02 17:05:54 +0100

    [_identity] added small syntax refactor: updated let to const

--- spec/main.spec.js
+++ spec/main.spec.js
@@ -19,45 +19,45 @@ describe('_.identity', function () {
         expect(_.identity).to.be.a('function');
     });
     it('returns the same value passed as an argument', function () {
-        let input = 'string';
-        let actual = _.identity(input);
-        let expected = 'string';
+        const input = 'string';
+        const actual = _.identity(input);
+        const expected = 'string';
 
         expect(actual).to.equal(expected);
     });
 
     describe('_.identity works for all primitive types', function () {
         it('booleans', function () {
-            let input = true;
-            let actual = _.identity(input);
-            let expected = true;
+            const input = true;
+            const actual = _.identity(input);
+            const expected = true;
 
             expect(actual).to.equal(expected);
         });
         it('numbers', function () {
-            let input = 1267672673;
-            let actual = _.identity(input);
-            let expected = 1267672673;
+            const input = 1267672673;
+            const actual = _.identity(input);
+            const expected = 1267672673;
 
             expect(actual).to.equal(expected);
         });
         it('string', function () {
-            let input = 'string';
-            let actual = _.identity(input);
-            let expected = 'string';
+            const input = 'string';
+            const actual = _.identity(input);
+            const expected = 'string';
 
             expect(actual).to.equal(expected);
         });
         it('null', function () {
-            let input = null;
-            let actual = _.identity(input);
-            let expected = null;
+            const input = null;
+            const actual = _.identity(input);
+            const expected = null;
 
             expect(actual).to.equal(expected);
         });
         it('undefined', function () {
-            let input;
-            let actual = _.identity(input);
+            const input = undefined;
+            const actual = _.identity(input);
 
             expect(actual).to.undefined;
         });
@@ -65,13 +65,13 @@ describe('_.identity', function () {
 
     describe('_.identity works for all objects', function () {
         it('objects', function () {
-            let input = {
+            const input = {
                 o: 'o',
                 b: 'b',
                 j: 'j',
             };
-            let actual = _.identity(input);
-            let expected = {
+            const actual = _.identity(input);
+            const expected = {
                 o: 'o',
                 b: 'b',
                 j: 'j',
@@ -79,15 +79,15 @@ describe('_.identity', function () {
             expect(actual).to.eql(expected);
         });
         it('functions', function () {
-            let input = function (param) { return param; };
-            let actual = _.identity(input);
+            const input = function (param) { return param; };
+            const actual = _.identity(input);
 
             expect(actual).to.equal(input);
         });
         it('arrays', function () {
-            let input = ['a', 'r', 'r', 'a', 'y'];
-            let actual = _.identity(input);
-            let expected = ['a', 'r', 'r', 'a', 'y'];
+            const input = ['a', 'r', 'r', 'a', 'y'];
+            const actual = _.identity(input);
+            const expected = ['a', 'r', 'r', 'a', 'y'];
 
             expect(actual).to.eql(expected);
         });
@@ -127,6 +127,7 @@ describe('_.first', function () {
         let expected = ['a', 'r', 'r', 'a', 'y'];
         expect(actual).to.eql(expected);
     });
+
 });
 describe('_.last', function () {
     it('should be a function', function () {
